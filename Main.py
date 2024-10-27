fb01bb2cf4f583163d6cae4f37409ae1a6e59763
LoverGreenTea <mgrican11@gmail.com>
2024-10-27 16:20:26 +0200

    test1

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 import os
-from PIL import Image
+from PIL import Image, ImageFilter
 import folder
 
 from PyQt5.QtGui import QImage, QPixmap
 from PyQt5.QtWidgets import *
+
 
 def pil2pixmap(im):
     if im.mode == "RGB":
@@ -20,11 +21,13 @@ def pil2pixmap(im):
     pixmap = QPixmap.fromImage(qim)
     return pixmap
 
+
 class PhotoManager:
     def __init__(self):
         self.photo = None
         self.folder = None
         self.filename = None
+        self.image_lbl = None
 
     def Downloader_photo(self):
         image_path = os.path.join(self.folder, self.filename)
@@ -35,7 +38,37 @@ class PhotoManager:
         pixels = pixels.scaledToWidth(500)
         image_lbl.setPixmap(pixels)
 
+    def Black_White(self, ):
+        self.photo = self.photo.convert("L")
+        self.show_photo(self.image_lbl)
 
+    def Left_side(self, ):
+        self.photo = self.photo.transpose(Image.ROTATE_90)
+        self.show_photo(self.image_lbl)
+
+    def Right_side(self, ):
+        self.photo = self.photo.transpose(Image.ROTATE_270)
+        self.show_photo(self.image_lbl)
+
+    def Mirror(self, ):
+        self.photo = self.photo.transpose(Image.FLIP_LEFT_RIGHT)
+        self.show_photo(self.image_lbl)
+
+    def light(self, ):
+        self.photo = self.photo.ImageEnhance.Brightness(self.photo).enhance(1.5)
+        self.show_photo(self.image_lbl)
+
+    def idk(self, ):
+        self.photo = self.photo.ImageEnhance.Contrast(self.photo).enhance(1.5)
+        self.show_photo(self.image_lbl)
+
+    def Filter(self, ):
+        self.photo = self.photo.filter(ImageFilter.EMBOSS )
+        self.show_photo(self.image_lbl)
+
+    def Blur(self, ):
+        self.photo = self.photo.filter(ImageFilter.BoxBlur(20))
+        self.show_photo(self.image_lbl)
 
 app = QApplication([])
 window = QWidget()
@@ -78,15 +111,21 @@ h3 = QHBoxLayout()
 h4 = QHBoxLayout()
 main_line = QHBoxLayout()
 
+window.resize(700, 700)
 
-window.resize(700,700)
-
+#пепший
 left = QPushButton("Вліво")
 right = QPushButton("Вправо")
 mirror = QPushButton("Дзеркало")
 light = QPushButton("Різкість")
 idk = QPushButton("Ч/Б")
 picture = QLabel("картинка")
+#другій
+test1 = QPushButton("Контрастність")
+test2 = QPushButton("Філтер")
+test3 = QPushButton("блюр")
+test4 = QPushButton("Б/Ч")
+test5 = QPushButton("test")
 
 failbut = QPushButton("Папка")
 piclist = QListWidget()
@@ -103,20 +142,45 @@ h3.addWidget(idk)
 
 v2.addLayout(h3)
 main_line.addLayout(v2)
+v2.addLayout(h4)
+
+h4.addWidget(test1)
+h4.addWidget(test2)
+h4.addWidget(test3)
+h4.addWidget(test4)
+h4.addWidget(test5)
 
 photo_manager = PhotoManager()
+photo_manager.image_lbl = picture
+
+
 def open_folder():
     photo_manager.folder = QFileDialog.getExistingDirectory()
     files = os.listdir(photo_manager.folder)
     piclist.clear()
     piclist.addItems(files)
 
+
 def show_chosen_image():
     photo_manager.filename = piclist.currentItem().text()
-    photo_manager.load()
-    photo_manager.show_photo(piclist)
+    photo_manager.Downloader_photo()
+    photo_manager.show_photo(picture)
+
+
 
 piclist.currentRowChanged.connect(show_chosen_image)
+
+idk.clicked.connect(photo_manager.Black_White)
+left.clicked.connect(photo_manager.Left_side)
+right.clicked.connect(photo_manager.Right_side)
+mirror.clicked.connect(photo_manager.Mirror)
+
+
+test1.clicked.connect(photo_manager.idk)
+test2.clicked.connect(photo_manager.Filter)
+test3.clicked.connect(photo_manager.Blur)
+test4.clicked.connect(photo_manager.WhiteBlack)
+
 failbut.clicked.connect(open_folder)
 window.setLayout(main_line)
 window.show()
